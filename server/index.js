b5b5ba50f6308d37103690e466cc7ae79502fbd0
Huy <skyzerzlol@gmail.com>
2019-07-14 14:49:27 +0300

    Ready to deploy

--- server/index.js
+++ server/index.js
@@ -65,12 +65,12 @@ app.use('/api/order', order);
 
 //Serve static asset in production
 
-if(process.env.NODE_ENV === 'production'){
+if(ENV === 'production'){
      //Set static folder
-     app.use(express.static('client/build'));
+     app.use(express.static(path.join(__dirname,'../client/build')));
 
-     app.get('*', (req,res)=>{
-         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+     app.get( (req,res)=>{
+         res.sendFile(path.join(__dirname, '../client/build/index.html'))
      })
 }
 app.listen(PORT, ()=>{
